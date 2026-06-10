04a2ee9fab5dcfdda73be563e1d0b9ef4f704ec4
Djeison Santos <djeison.adventista@gmail.com>
2026-06-10 18:07:39 -0100

    fix: correct syntax and comments in page.tsx for clarity

--- servidor-local-front/app/(aps)/new-request/page.tsx
+++ servidor-local-front/app/(aps)/new-request/page.tsx
@@ -1,26 +1,26 @@
-"use client"
-import dynamic from 'next/dynamic'
+"use client"; 
 
-// Carrega o Header apenas no navegador, ignorando o teste de build do servidor
+import dynamic from 'next/dynamic';
+
+
 const HeaderSection = dynamic(
     () => import("@/components/request/header-section").then(mod => mod.HeaderSection),
     { ssr: false }
-)
+);
 
-// Carrega a Section apenas no navegador, ignorando o teste de build do servidor
+
 const RequestSection = dynamic(
     () => import("@/components/request/request-section").then(mod => mod.RequestSection),
     { ssr: false }
-)
+);
 
 const Page = () => {
     return (
         <div className="min-h-screen bg-[#F5F7FA]">
             <HeaderSection />
-
             <RequestSection />
         </div>
-    )
-}
+    );
+};
 
-export default Page
+export default Page;
